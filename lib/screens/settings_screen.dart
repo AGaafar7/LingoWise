ea32b886e92a88b0a487f1b71b73d2c8ffb2bf21
AGaafar7 <a.gaafar.junior@gmail.com>
2025-03-23 23:24:12 +0200

    adding specific settings tiles

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -22,15 +22,45 @@ class SettingsScreen extends StatelessWidget {
           children: [
             Expanded(
               child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
                 itemBuilder: (context, index) {
-                  //TODO: Create two lists of icons and titles and subtitle and the trailing icon
+                  final List<IconData> leadingIcons = [
+                    Icons.account_circle_rounded,
+                    Icons.privacy_tip_rounded,
+                    Icons.chat_bubble_rounded,
+                    Icons.cloud_done_rounded,
+                    Icons.help_rounded,
+                    Icons.notifications_active_rounded,
+                    Icons.people_rounded,
+                  ];
+                  final List<String> titles = [
+                    "Account",
+                    "Privacy",
+                    "Chat",
+                    "Storage and Data",
+                    "Help",
+                    "Notification",
+                    "Invite Friends",
+                  ];
+                  final List<String> tilesSubtitle = [
+                    "My number, delete my account, change number, change subscription",
+                    "Blocked contacts",
+                    "app lock, hide chat",
+                    "Language, theme, chat background",
+                    "Media download, network usage",
+                    "Privacy policy, terms & conditions",
+                    "Messages, calls & group alerts",
+                    "",
+                  ];
                   return SettingsTile(
-                    leadingIcon: Icon(Icons.account_circle_rounded),
-                    tileTitle: "Account",
-                    tileSubtitle: "My Number",
+                    leadingIcon: Icon(leadingIcons[index]),
+                    tileTitle: titles[index],
+                    tileSubtitle: tilesSubtitle[index],
                     trailingIcon: Icon(Icons.arrow_forward_ios_rounded),
                   );
                 },
+                itemCount: 6,
               ),
             ),
           ],
