c6477cd424c498127335a15bee525c96aff13b19
AGaafar7 <a.gaafar.junior@gmail.com>
2025-03-28 22:34:22 +0200

    removing import

--- lib/screens/auth/register_screen.dart
+++ lib/screens/auth/register_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:lingowise/screens/screens.dart';
-import 'login_screen.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
